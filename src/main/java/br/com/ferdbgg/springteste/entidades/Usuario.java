f180cde85b717edf22d5e733cdc92c8aa4b5a011
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-12-15 10:43:10 -0300

    faz configuração do banco h2

--- src/main/java/br/com/ferdbgg/springteste/entidades/Usuario.java
+++ src/main/java/br/com/ferdbgg/springteste/entidades/Usuario.java
@@ -2,10 +2,17 @@ package br.com.ferdbgg.springteste.entidades;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="usuario")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,6 +20,8 @@ public class Usuario implements Serializable{
 
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
