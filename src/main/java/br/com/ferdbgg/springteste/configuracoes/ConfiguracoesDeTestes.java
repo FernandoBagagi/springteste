86b0d0a980bf49700aa94835c7add213aa282713
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-12-19 08:55:59 -0300

    cria novos dados pra teste

--- src/main/java/br/com/ferdbgg/springteste/configuracoes/ConfiguracoesDeTestes.java
+++ src/main/java/br/com/ferdbgg/springteste/configuracoes/ConfiguracoesDeTestes.java
@@ -1,5 +1,6 @@
 package br.com.ferdbgg.springteste.configuracoes;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.ferdbgg.springteste.entities.Pedido;
 import br.com.ferdbgg.springteste.entities.Usuario;
+import br.com.ferdbgg.springteste.entities.enums.StatusPedido;
+import br.com.ferdbgg.springteste.repositories.PedidoRepository;
 import br.com.ferdbgg.springteste.repositories.UsuarioRepository;
 
 @Configuration
@@ -17,6 +21,8 @@ public class ConfiguracoesDeTestes implements CommandLineRunner{
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,7 +32,13 @@ public class ConfiguracoesDeTestes implements CommandLineRunner{
         usuariosDumb.add(new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123457", null));
         usuariosDumb.add(new Usuario(null, "Carlos José", "carlos@gmail.com", null, "123458", null));
 
+        List<Pedido> pedidosDumb = new ArrayList<>();
+        pedidosDumb.add(new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), usuariosDumb.get(0), StatusPedido.AGUARDANDO_PAGAMENTO.getCodigo()));
+        pedidosDumb.add(new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), usuariosDumb.get(1), StatusPedido.CANCELADO.getCodigo()));
+        pedidosDumb.add(new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), usuariosDumb.get(0), StatusPedido.ENTREGUE.getCodigo()));
+
         this.usuarioRepository.saveAll(usuariosDumb);
+        this.pedidoRepository.saveAll(pedidosDumb);
     }
     
 }
