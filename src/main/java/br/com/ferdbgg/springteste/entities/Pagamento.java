9cbfb6cb455542c0f3f26c0b93742be29d155900
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-12-26 09:48:19 -0300

    cria Pagamento

---
+++ src/main/java/br/com/ferdbgg/springteste/entities/Pagamento.java
@@ -0,0 +1,41 @@
+package br.com.ferdbgg.springteste.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="pagamentos")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagamento implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant instante;
+
+    @OneToOne
+    @MapsId
+    private Pedido pedido;
+
+}
